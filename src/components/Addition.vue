eb1f44a8ec9e4026d18df15f1debbc20d8baaf63
gkzzz <1215115833@qq.com>
2019-09-19 23:35:03 +0800

    backup

---
+++ src/components/Addition.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <!-- <h3>当前最新的count值为: {{$store.state.count}}</h3> -->
+        <h3>{{$store.getters.showNum}}</h3>
+        <button @click="handle1">+1</button>
+        <button @click="handle2">+N</button>
+        <button @click="handle3">+1 Async</button>
+        <button @click="handle4">+N Async</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    },
+    methods: {
+        handle1() {
+            // commit的作用就是调用某个mutation的函数
+            this.$store.commit('add')
+        },
+        handle2() {
+            this.$store.commit('addN', 3)
+        },
+        // 异步的让count自增1
+        handle3() {
+            // dispatch函数专门用来触发action
+            this.$store.dispatch('addAsync')
+        },
+        handle4() {
+            this.$store.dispatch('addNAsync', 2)
+        }
+    }
+}
+</script>
--- src/store.js
+++ src/store.js
@@ -4,13 +4,54 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 访问数据用 state
   state: {
-
+    count: 0
   },
+  // 修改数据用 mutation
   mutations: {
-
+    add(state) {
+      state.count++
+    },
+    addN(state, step) {
+      state.count += step
+    },
+    sub(state) {
+      state.count--
+    },
+    subN(state, step) {
+      state.count -= step
+    },
   },
+  // 处理异步任务用 action
   actions: {
-
+    addAsync(context) {
+      setTimeout(() => {
+        // actions不能修改state中的数据
+        // 必须通过context.commit触发某个mutation才行
+        context.commit('add')
+      }, 1000)
+    },
+    addNAsync(context, step) {
+      setTimeout(() => {
+        context.commit('addN', step)
+      }, 1000)
+    },
+    subAsync(context) {
+      setTimeout(() => {
+        context.commit('sub')
+      }, 1000)
+    },
+    subNAsync(context, step) {
+      setTimeout(() => {
+        context.commit('subN', step)
+      }, 1000)
+    }
+  },
+  // 加工处理形成新数据用 getter
+  getters: {
+    showNum(state) {
+      return '当前最新的数量是【' + state.count + '】'
+    }
   }
 })
